096ad44ed8541598c46afa401dd69dc34e4347bb
jaylzv <jaylzv7@gmail.com>
2025-02-09 10:40:00 +0100

    make code more modular, functional, add template for future code

---
+++ utils/test-browsers.ts
@@ -0,0 +1,59 @@
+import { chromium, firefox, webkit, Browser } from "@playwright/test";
+import { BrowsersEnum, BrowsersType } from "./consts";
+
+/**
+ * Launches a browser instance based on the specified browser type.
+ *
+ * @param {BrowserType} browser - The type of browser to launch. Must be one of the values from `BrowsersEnum`.
+ * @returns {Promise<Browser>} - A promise that resolves to the launched `Browser` instance.
+ *
+ * @example
+ * ```typescript
+ * const browser = await launchBrowserInstance(BrowsersEnum.CHROMIUM);
+ * ```
+ */
+const launchBrowserInstance = async (
+  browser: BrowsersType
+): Promise<Browser> => {
+  return await {
+    [BrowsersEnum.CHROMIUM]: chromium,
+    [BrowsersEnum.FIREFOX]: firefox,
+    [BrowsersEnum.WEBKIT]: webkit,
+  }[browser].launch({ headless: false });
+};
+
+/**
+ * TODO: Update docs.
+ * Launches a browser instance, navigates to a specified URL, waits for a specified timeout, and then closes the browser.
+ *
+ * @param {BrowsersType} browser - The type of browser to launch.
+ * @returns {Promise<void>} - A promise that resolves when the browser instance is closed.
+ */
+const testBrowser = async (browser: BrowsersType): Promise<void> => {
+  const browserInstance = await launchBrowserInstance(browser);
+  const page = await browserInstance.newPage();
+
+  await page.goto("https://example.com");
+  await page.waitForTimeout(60000);
+
+  await browserInstance.close();
+};
+
+/**
+ * TODO: Update docs.
+ * Tests all supported browsers sequentially.
+ *
+ * This function will run tests on the following browsers:
+ * - Chromium
+ * - Firefox
+ * - WebKit
+ *
+ * @returns {Promise<void>} A promise that resolves when all browser tests are complete.
+ */
+const testAllBrowsers = async (): Promise<void> => {
+  await testBrowser(BrowsersEnum.CHROMIUM);
+  await testBrowser(BrowsersEnum.FIREFOX);
+  await testBrowser(BrowsersEnum.WEBKIT);
+};
+
+export default testAllBrowsers;
