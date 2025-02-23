8cdc654e309c5c57822e0318e6d3662b24ab4656
jaylzv <jaylzv7@gmail.com>
2025-02-23 20:45:06 +0100

    only test extensions for chromium

--- utils/testing.ts
+++ utils/testing.ts
@@ -10,18 +10,41 @@ const testFingerprinting = async () => {
   for (const browserName of BROWSERS_NAMES) {
     console.log(`Launching browser ${browserName} instance...`);
 
-    for (const extensionCombination of EXTENSION_COMBINATIONS) {
-      let extensionPaths: string[] = [];
-      console.log(`Extension combination: ${extensionCombination}`);
-      for (const extension of extensionCombination) {
-        extensionPaths.push(EXTENSION_PATHS.get(extension)!);
-      }
+    if (browserName === "chromium") {
+      // Extensions in Playwright are only supported in Chromium.
+      for (const extensionCombination of EXTENSION_COMBINATIONS) {
+        let extensionPaths: string[] = [];
+        console.log(`Extension combination: ${extensionCombination}`);
+        for (const extension of extensionCombination) {
+          extensionPaths.push(EXTENSION_PATHS.get(extension)!);
+        }
 
-      const browserInstance = await launchBrowserInstance(
-        browserName,
-        extensionPaths.join(",")
-      );
-      console.log("Installed extension combination.");
+        const browserInstance = await launchBrowserInstance(
+          browserName,
+          extensionPaths.join(",")
+        );
+        console.log("Installed extension combination.");
+        console.log(`Launched ${browserName} browser instance.`);
+
+        const page = await browserInstance.newPage();
+
+        for (const searchEngine of SEARCH_ENGINES) {
+          console.log(`Navigating to search engine ${searchEngine}...`);
+          await navigateToSearchEngine(page, searchEngine);
+          console.log(`Navigated to ${searchEngine}.`);
+
+          await explicitlyDenyCookies(page, searchEngine);
+
+          await page.waitForTimeout(1000);
+        }
+
+        console.log(`Closing browser ${browserName} instance...`);
+        await browserInstance.browser()!.close();
+      }
+    } else {
+      // Extensions are not supported in Firefox and WebKit.
+      console.log(`Launching browser ${browserName} instance...`);
+      const browserInstance = await launchBrowserInstance(browserName);
       console.log(`Launched ${browserName} browser instance.`);
 
       const page = await browserInstance.newPage();
