2a5fa13eae9b35a2173b7925216f7508ef75e850
jaylzv <jaylzv7@gmail.com>
2025-03-01 11:55:13 +0100

    add interfaces for fingeprint data, currently with errors

--- tests/fingerprinting/types.ts
+++ tests/fingerprinting/types.ts
@@ -1,12 +1,64 @@
 import { Page } from "@playwright/test";
+import { ExtensionType } from "../../utils/extensions/types";
 
-export interface FingerprintDataType {
-  // TODO: Implement
-  // This will be the data structure.
-  TODO: "Implement";
-}
-
-export interface FingerprintSiteOptionsType {
+interface FingerprintSiteOptionsType {
   page: Page;
   siteUrl: string;
 }
+
+export { FingerprintSiteOptionsType };
+
+interface FingerprintDataLocationType {
+  location: { country: string; region: string; city: string };
+  locationLatitude: number;
+  locationLongtitude: number;
+  postalCode: number;
+  timeZone: string;
+  geolocationOffset: number;
+}
+
+interface FingerprintDataNetworkType {
+  ip: string;
+  dns: string;
+  webRTC: string;
+  isp: string;
+  httpData: undefined; // TODO: type: HttpDataType; How to go on about this?
+  dntActive: boolean;
+}
+
+interface FingerprintDataBrowserType {
+  browserName: string;
+  browserVersion: string;
+  userAgent: string;
+  extensions: ExtensionType[];
+  javascriptEnabled: boolean;
+  activeXEnabled: boolean;
+  flashEnabled: boolean;
+  cookieEnabled: boolean;
+  contentLanguage: string;
+  fonts: string[];
+  webGLData: undefined; // TODO: type: WebGLDataType; How to go on about this?
+  incognitoEnabled: boolean;
+}
+
+interface FingerprintDataHardwareType {
+  screenResolution: { width: number; height: number };
+  availableScreenSize: { width: number; height: number };
+  colorDepth: number;
+  touchSupportEnabled: number;
+  deviceMemory: number;
+  concurrency: number; // What is 'concurrency'?
+  cpuCores: number;
+  graphicsCardName: string;
+  touchScreenEnabled: boolean;
+}
+
+interface FingerprintDataType {
+  operatingSystem: string;
+  location: FingerprintDataLocationType;
+  network: FingerprintDataNetworkType;
+  browser: FingerprintDataBrowserType;
+  hardware: FingerprintDataHardwareType;
+}
+
+export { FingerprintDataType };
