aab4956e37cce98b2ae31c1a0dcb5329f5c09806
jaylzv <jaylzv7@gmail.com>
2025-03-02 20:06:24 +0100

    add skeleton for whoer data retrieval

--- tests/fingerprinting/sites/whoer.ts
+++ tests/fingerprinting/sites/whoer.ts
@@ -1,13 +1,61 @@
+import { Page } from "@playwright/test";
 import {
-  DUMMY_FINGERPRINT_DATA,
   FingerprintDataType,
   FingerprintSiteOptionsType,
+  FingerprintDataLocationType,
+  FingerprintDataNetworkType,
+  FingerprintDataBrowserType,
+  FingerprintDataHardwareType,
+  DUMMY_FINGERPRINT_DATA,
 } from "../types";
+
+const retrieveLocationData = async (
+  page: Page
+): Promise<FingerprintDataLocationType> => {
+  return DUMMY_FINGERPRINT_DATA.location; // TODO: Placeholder, remove!
+};
+
+const retrieveNetworkData = async (
+  page: Page
+): Promise<FingerprintDataNetworkType> => {
+  return DUMMY_FINGERPRINT_DATA.network; // TODO: Placeholder, remove!
+};
+
+const retrieveBrowserData = async (
+  page: Page
+): Promise<FingerprintDataBrowserType> => {
+  return DUMMY_FINGERPRINT_DATA.browser; // TODO: Placeholder, remove!
+};
+
+const retrieveHardwareData = async (
+  page: Page
+): Promise<FingerprintDataHardwareType> => {
+  return DUMMY_FINGERPRINT_DATA.hardware; // TODO: Placeholder, remove!
+};
 
 const retrieveWhoerFingerprintData = async (
   options: FingerprintSiteOptionsType
 ): Promise<FingerprintDataType> => {
-  return DUMMY_FINGERPRINT_DATA;
+  const { page, siteUrl } = options;
+
+  await page.goto(siteUrl);
+  await page.waitForLoadState("domcontentloaded");
+  await page.waitForLoadState("networkidle"); // TODO: This is deprecated, remove later.
+
+  const locationData = await retrieveLocationData(page);
+  const networkData = await retrieveNetworkData(page);
+  const browserData = await retrieveBrowserData(page);
+  const hardwareData = await retrieveHardwareData(page);
+
+  const whoerData: FingerprintDataType = {
+    operatingSystem: "TODO: Update.",
+    location: locationData,
+    network: networkData,
+    browser: browserData,
+    hardware: hardwareData,
+  };
+
+  return whoerData;
 };
 
 export { retrieveWhoerFingerprintData };
