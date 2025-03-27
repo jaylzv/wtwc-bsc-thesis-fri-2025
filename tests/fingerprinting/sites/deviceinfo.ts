855de62f99caf8169fa124be78ebc6a21891b40b
Jordan Lazov <118006842+jaylzv@users.noreply.github.com>
2025-03-27 23:46:21 +0100

    add deviceinfo.me retrieval of data

--- tests/fingerprinting/sites/deviceinfo.ts
+++ tests/fingerprinting/sites/deviceinfo.ts
@@ -1,6 +1,5 @@
 import { Page } from "@playwright/test";
 import {
-  DUMMY_FINGERPRINT_DATA,
   FingerprintDataType,
   FingerprintSiteOptionsType,
   FingerprintDataBrowserType,
@@ -8,9 +7,17 @@ import {
   FingerprintDataLocationType,
   FingerprintDataNetworkType,
   LocationType,
+  ViewportType,
 } from "../types";
 import { properlyNavigateToURL } from "../../../utils/general-utils";
 
+/**
+ * Retrieves data for a specific text selector on the page.
+ *
+ * @param {Page} page - The Playwright Page object representing the browser page.
+ * @param {string} textSelector - The text selector to locate the data.
+ * @returns {Promise<string>} A promise that resolves to the retrieved data as a string.
+ */
 const retrieveDataForTextSelector = async (
   page: Page,
   textSelector: string
@@ -28,6 +35,12 @@ const retrieveDataForTextSelector = async (
   return retrievedData;
 };
 
+/**
+ * Retrieves location-related fingerprint data from deviceinfo.me.
+ *
+ * @param {Page} page - The Playwright Page object representing the browser page.
+ * @returns {Promise<FingerprintDataLocationType>} A promise that resolves to an object containing location data.
+ */
 const retrieveLocationData = async (
   page: Page
 ): Promise<FingerprintDataLocationType> => {
@@ -57,24 +70,148 @@ const retrieveLocationData = async (
   return locationData;
 };
 
+/**
+ * Retrieves network-related fingerprint data from deviceinfo.me.
+ *
+ * @param {Page} page - The Playwright Page object representing the browser page.
+ * @returns {Promise<FingerprintDataNetworkType>} A promise that resolves to an object containing network data.
+ */
 const retrieveNetworkData = async (
   page: Page
 ): Promise<FingerprintDataNetworkType> => {
-  return DUMMY_FINGERPRINT_DATA.network;
+  const ip = await retrieveDataForTextSelector(page, "IP Address (WAN):");
+  const webRTC = await retrieveDataForTextSelector(page, "WebRTC:");
+  const isp = await retrieveDataForTextSelector(page, "ISP:");
+  const dntActive =
+    (await retrieveDataForTextSelector(page, "Do Not Track:")) === "Enabled";
+
+  const networkData: FingerprintDataNetworkType = {
+    ip,
+    dns: null, // TODO: Implement.
+    webRTC,
+    isp,
+    httpData: null, // TODO: Implement.
+    dntActive,
+  };
+
+  return networkData;
 };
 
+/**
+ * Retrieves browser-related fingerprint data from deviceinfo.me.
+ *
+ * @param {Page} page - The Playwright Page object representing the browser page.
+ * @returns {Promise<FingerprintDataBrowserType>} A promise that resolves to an object containing browser data.
+ */
 const retrieveBrowserData = async (
   page: Page
 ): Promise<FingerprintDataBrowserType> => {
-  return DUMMY_FINGERPRINT_DATA.browser;
+  const name = await retrieveDataForTextSelector(page, "Browser:");
+  const userAgent = await retrieveDataForTextSelector(page, "User Agent:");
+  const extensions = [await retrieveDataForTextSelector(page, "Extensions:")];
+  const contentLanguage = await retrieveDataForTextSelector(page, "Languages:");
+
+  const javascriptEnabled =
+    (await retrieveDataForTextSelector(page, "JavaScript:")) === "Enabled";
+  const activeXEnabled =
+    (await retrieveDataForTextSelector(page, "ActiveX:")) === "Supported";
+  const cookiesEnabled =
+    (await retrieveDataForTextSelector(page, "Cookies:")) === "Enabled";
+
+  // TODO: Specific-utils.ts?
+  const versionText = await retrieveDataForTextSelector(page, "Browser:");
+  const versionMatch = versionText
+    .split(" ")
+    .find((word, index, arr) => arr[index - 1] === "version");
+  const version = versionMatch || null;
+
+  const browserData: FingerprintDataBrowserType = {
+    name,
+    version,
+    userAgent,
+    extensions,
+    javascriptEnabled,
+    activeXEnabled,
+    flashEnabled: null, // Not supported by deviceinfo.me
+    cookiesEnabled,
+    contentLanguage,
+    fonts: null, // TODO: Extra step needed.
+    webGLData: null, // TODO: Implement.
+    incognitoEnabled: null, // Not supported by deviceinfo.me
+  };
+
+  return browserData;
 };
 
+/**
+ * Retrieves hardware-related fingerprint data from deviceinfo.me.
+ *
+ * @param {Page} page - The Playwright Page object representing the browser page.
+ * @returns {Promise<FingerprintDataHardwareType>} A promise that resolves to an object containing hardware data.
+ */
 const retrieveHardwareData = async (
   page: Page
 ): Promise<FingerprintDataHardwareType> => {
-  return DUMMY_FINGERPRINT_DATA.hardware;
+  // TODO: Special utils?
+  const unparsedResolution = await retrieveDataForTextSelector(
+    page,
+    "Resolution:"
+  );
+  const width = parseInt(unparsedResolution.split(" ")[0]);
+  const height = parseInt(unparsedResolution.split(" ")[2]);
+  const screenResolution: ViewportType = { width, height };
+
+  const colorDepthUnparsed = await retrieveDataForTextSelector(
+    page,
+    "Color Depth:"
+  );
+  const colorDepth = parseInt(colorDepthUnparsed.replace("-bit", ""));
+
+  const deviceMemory = parseInt(
+    await retrieveDataForTextSelector(page, "Memory (RAM):")
+  );
+  const cpuCores = parseInt(
+    await retrieveDataForTextSelector(page, "Number of cores:")
+  );
+
+  const graphicsCard = await retrieveDataForTextSelector(
+    page,
+    "Graphics Card Name / Driver:"
+  );
+
+  const touchScreenEnabled =
+    (await retrieveDataForTextSelector(page, "Touchscreen:")) === "Yes";
+
+  const hardwareData: FingerprintDataHardwareType = {
+    screenResolution,
+    availableScreenSize: null, // Not provided by deviceinfo.me
+    colorDepth,
+    deviceMemory,
+    concurrency: null, // Not provided by deviceinfo.me
+    cpuCores,
+    graphicsCard,
+    touchScreenEnabled,
+  };
+
+  return hardwareData;
 };
 
+/**
+ * Retrieves fingerprint data from deviceinfo.me.
+ *
+ * @param {FingerprintSiteOptionsType} options - The options for the fingerprint site, including the page and site URL.
+ * @returns {Promise<FingerprintDataType>} A promise that resolves to the fingerprint data.
+ *
+ * @example
+ * ```typescript
+ * const options: FingerprintSiteOptionsType = {
+ *   page: browserPage,
+ *   siteUrl: "https://deviceinfo.me"
+ * };
+ * const fingerprintData = await retrieveDeviceInfoFingerprintData(options);
+ * console.log(fingerprintData);
+ * ```
+ */
 const retrieveDeviceInfoFingerprintData = async (
   options: FingerprintSiteOptionsType
 ): Promise<FingerprintDataType> => {
@@ -82,7 +219,10 @@ const retrieveDeviceInfoFingerprintData = async (
 
   await properlyNavigateToURL(page, siteUrl);
 
-  const operatingSystem = "TODO: Implement.";
+  const operatingSystem = await retrieveDataForTextSelector(
+    page,
+    "Operating System:"
+  );
   const locationData = await retrieveLocationData(page);
   const networkData = await retrieveNetworkData(page);
   const browserData = await retrieveBrowserData(page);
