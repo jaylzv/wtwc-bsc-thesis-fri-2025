7083fcc3d297b7fae37d2fd52a72846b51e3e7a6
jaylzv <jaylzv7@gmail.com>
2025-04-19 10:34:43 +0200

    remove deviceinfo as it's unreliable

--- tests/fingerprinting/consts.ts
+++ tests/fingerprinting/consts.ts
@@ -1,8 +1,5 @@
-const FINGERPRINTING_SITES_URLS: string[] = [
-  "https://www.browserscan.net/",
-  "https://www.deviceinfo.me/",
-];
+const FINGERPRINTING_SITES_URLS: string[] = ["https://www.browserscan.net/"];
 
-const FINGERPRINTING_WEBSITE_CLI_ARGS: string[] = ["browserscan", "deviceinfo"];
+const FINGERPRINTING_WEBSITE_CLI_ARGS: string[] = ["browserscan"];
 
 export { FINGERPRINTING_SITES_URLS, FINGERPRINTING_WEBSITE_CLI_ARGS };
--- tests/fingerprinting/sites/index.ts
+++ tests/fingerprinting/sites/index.ts
@@ -1,2 +1,1 @@
-export * from "./deviceinfo";
 export * from "./browserscan";
